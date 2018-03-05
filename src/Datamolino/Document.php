285a8b3c3bd999d1b786f90dab5ee78628dc16b4
Cybervitexus <info@vitexsoftware.cz>
2018-03-05 15:50:09 +0100

    Get OriginalDocumentData & SaveOriginal Document to file functions added

--- src/Datamolino/Document.php
+++ src/Datamolino/Document.php
@@ -43,8 +43,43 @@ class Document extends ApiClient
         parent::__construct($init, $options);
     }
 
+    /**
+     * Obtain Documents listing for given folder
+     * 
+     * @param int $agendaId FolderID
+     * 
+     * @return array
+     */
     public function getDocuments($agendaId)
     {
         return $this->requestData('?agenda_id='.$agendaId);
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function getOriginalFileData($documentId = null)
+    {
+        if (is_null($documentId)) {
+            $documentId = $this->getMyKey();
+        }
+        return current($this->requestData($documentId.'/original_file'));
+    }
+
+    /**
+     * Save original document to file
+     * 
+     * @param string $destination Dirpath or filename
+     * @param int    $documentId  Datamolino document ID
+     * 
+     * @return int saved data length
+     */
+    public function saveOriginalFile($destination, $documentId = null)
+    {
+        $originalFileData = $this->getOriginalFileData($documentId);
+        return file_put_contents(is_dir($destination) ? $destination.'/'.$originalFileData['user_file_name']
+                : $originalFileData['user_file_name'],
+            base64_decode($originalFileData['original_file_base64']));
     }
 }
